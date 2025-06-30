b7809fead0bff2dc00b025a29e98727849e728d5
Marvin Andrew Rosales <marvinandrew.rosales@GXCHLAP00375local.local>
2025-06-30 14:51:07 +0800

    Initial submit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Marvin Andrew S. Rosales
+# Lab 1: Celsius to Fahrenheit Converter
+
+Sample Output 1:
+![img.png](img.png)
+
+Sample Output 2:
+![img_1.png](img_1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+import java.io.InputStream;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Enter temperature in Celsius:");
+
+        Scanner input = new Scanner(System.in);
+        float celsiusInput = input.nextFloat();
+//        System.out.print(celsiusInput);
+//        float celsiusInput = Float.pa
+        float FahrenheitOutput = (float) ((celsiusInput * (9.0/5.0) ) + 32);
+
+        System.out.print("Temperature in Fahrenheit: ");
+        System.out.printf("%.2f",FahrenheitOutput);
+
 
     }
 }
